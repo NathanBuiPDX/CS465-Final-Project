348ebac4325278e8d618841c25a323e46359ac2b
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-11-15 05:58:02 +0530

    using bcrypt created salt and new hashed password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // REGISTER USER
 router.post('/register', async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
+
     try {
+        const hashedPassword = await generateHashedPwd(req.body.password);
+        const newUser = createNewUser(req.body.username, hashedPassword);
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (error) {
@@ -16,3 +16,16 @@ router.post('/register', async (req, res) => {
 });
 
 module.exports = router;
+
+function createNewUser(username, password) {
+    return new User({
+        username: username,
+        password: password
+    });
+}
+
+async function generateHashedPwd(password) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
