4b881e6d8b12f9eee1ec34ef407fe5f91fa4e117
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-11-17 04:23:38 +0530

    refactor register api for taking all parameters and added login api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,30 +2,41 @@ const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
-// REGISTER USER
-router.post('/register', async (req, res) => {
-
+router.post('/login', async (req, res) => {
     try {
-        const hashedPassword = await generateHashedPwd(req.body.password);
-        const newUser = createNewUser(req.body.username, hashedPassword);
-        const user = await newUser.save();
+        const loginId = req.body.username;
+        const user = await User.findOne({ username: loginId });
+        if (!user) res.status(404).json(`User not found with Username : ${loginId}`);
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) res.status(400).json(`Wrong Password!`);
+
         res.status(200).json(user);
     } catch (error) {
         console.error(error);
     }
 });
 
-module.exports = router;
-
-function createNewUser(username, password) {
-    return new User({
-        username: username,
-        password: password
-    });
-}
+// REGISTER USER
+router.post('/register', async (req, res) => {
+    try {
+        if (req.body.username && req.body.password && req.body.name && req.body.full_name) {
+            req.body.password = await generateHashedPwd(req.body.password);
+            const newUser = new User(req.body);
+            const user = await newUser.save();
+            res.status(200).json(user);
+        } else {
+            res.status(400).json('Required field is empty');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 async function generateHashedPwd(password) {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     return hashedPassword;
 }
+
+module.exports = router;
