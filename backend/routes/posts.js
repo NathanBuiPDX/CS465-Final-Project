969138f39a61dc5b88b186f6b31711ec968d8a95
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-11-18 06:01:46 +0530

    new post, update, get all post of current user, get all post of another user, get post by postid, delete a post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Post = require('../models/Post');
 const cookie = require("./cookie");
 
-// WRITE A NEW POST
+// WRITE A NEW POST BY CURRENT USER
 router.post('/', async (req, res) => {
     try {
         const cookies = cookie.fetchCookies(req);
@@ -34,6 +34,83 @@ router.put('/:postId', async (req, res) => {
             }
             await Post.findByIdAndUpdate({ _id: postId }, { $set: req.body });
             res.status(200).json('Post Updated');
+        } else {
+            res.status(403).json('Unauthorized user');
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// GET ALL POST OF CURRENT USER
+router.get('/', async (req, res) => {
+    try {
+        const cookies = cookie.fetchCookies(req);
+        if (cookies['userId']) {
+            const postList = await Post.find({ user_id: cookies['userId'] });
+            if (!postList) {
+                res.status(200).json(`No Post found`);
+                return;
+            }
+            res.status(200).json(postList);
+        } else {
+            res.status(403).json('Unauthorized user');
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// GET ALL POST OF ANOTHER USER
+router.get('/users/:userId', async (req, res) => {
+    try {
+        const cookies = cookie.fetchCookies(req);
+        const userId = req.params.userId;
+        if (cookies['userId']) {
+            const postList = await Post.find({ user_id: userId }, { __v: 0 });
+            if (!postList) {
+                res.status(200).json(`No Post found`);
+                return;
+            }
+            res.status(200).json(postList);
+        } else {
+            res.status(403).json('Unauthorized user');
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// GET A POST BY POSTID
+router.get('/:postId', async (req, res) => {
+    try {
+        const cookies = cookie.fetchCookies(req);
+        const postId = req.params.postId;
+        if (cookies['userId']) {
+            const post = await Post.findById(postId);
+            if (!post) {
+                res.status(404).json(`Post not found`);
+                return;
+            }
+            // eslint-disable-next-line no-unused-vars
+            const { __v, ...other } = post._doc;
+            res.status(200).json(other);
+        } else {
+            res.status(403).json('Unauthorized user');
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// DELETE A POST
+router.delete('/:postId', async (req, res) => {
+    try {
+        const cookies = cookie.fetchCookies(req);
+        const postId = req.params.postId;
+        if (cookies['userId']) {
+            await Post.findByIdAndDelete(postId);
+            res.status(200).json('Post deleted');
         } else {
             res.status(403).json('Unauthorized user');
         }
