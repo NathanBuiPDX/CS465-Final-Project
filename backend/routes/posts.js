c3f19d0168727a57dc70967a31c87b55b2afcb7e
huutribui <huu-tri.bui@intel.com>
2022-11-25 17:03:11 -0800

    Modified the multer in index.js and added checking condition to check if the user create a post with/without image

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,22 +2,20 @@ const router = require('express').Router();
 const Post = require('../models/Post');
 const cookie = require("./cookie");
 // multer takes the image data and stores it in memory
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
 const s3Function = require('../s3helper/bucket')
 // WRITE A NEW POST BY CURRENT USE
-router.post("/", upload.single('imageFile'), async (req, res) => {
+router.post("/",  async (req, res) => {
   try {
     const cookies = cookie.fetchCookies(req);
     if (cookies["userId"]) {
       req.body.user_id = cookies["userId"];
       // AWS S3 LOGIC
       // req.file.buffer is our image that we send to S3
-      const pic =  req.file.buffer;
+      const pic =  req?.file?.buffer;
 
       console.log("FILE: ", pic);
-      // post image to S3BUCKET
+      // post image to S3BUCKET if the user did include an image
       if (pic) {
                 var s3Object = await s3Function.setImage(pic);
                 // so we can store our s3Unique key in our DB
@@ -30,9 +28,12 @@ router.post("/", upload.single('imageFile'), async (req, res) => {
       // eslint-disable-next-line no-unused-vars
       const { createdAt, updatedAt, __v, ...other } = post._doc;
 
+      //if the pic is included, then extract the s3 URL and save it to image_url
       // store s3URL to frontEnd because we want to store the key in DB
-      const s3pic = s3Object.url;
-      other.image_url = s3pic
+      if (pic) {
+        const s3pic = s3Object.url;
+        other.image_url = s3pic
+      }
       res.status(200).json(other);
     } else {
       res.status(403).json("Unauthorized user");
